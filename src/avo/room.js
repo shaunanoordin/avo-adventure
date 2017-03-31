7c32e2679674e96d4ca2ffbc44be5f640f33b820
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-03-31 03:19:12 +0100

    Prepared a room...

---
+++ src/avo/room.js
@@ -0,0 +1,13 @@
+/*  
+Room
+====
+
+(Shaun A. Noordin || shaunanoordin.com || 20170331)
+********************************************************************************
+ */
+
+export class Room {
+  constructor() {
+    
+  }
+}
